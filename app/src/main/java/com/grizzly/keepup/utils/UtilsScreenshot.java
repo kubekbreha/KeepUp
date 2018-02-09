a1fec3fac69c3d758832dc4555c8fcd32d2b8309
Jakub Brehuv <jakub.brehuv@student.tuke.sk>
2018-02-09 08:31:11 +0100

    copyright added

--- app/src/main/java/com/grizzly/keepup/utils/UtilsScreenshot.java
+++ app/src/main/java/com/grizzly/keepup/utils/UtilsScreenshot.java
@@ -1,8 +1,21 @@
+/*
+* Copyright 2018 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package com.grizzly.keepup.utils;
 
-/**
- * Created by kubek on 1/26/18.
- */
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
@@ -10,6 +23,10 @@ import android.view.View;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+
+/**
+ * Created by kubek on 1/26/18.
+ */
 
 public class UtilsScreenshot {
 
